e9c74f90580e58166ac36e72e6f936897f7ee611
sreeharsha-rav <dev.sreeharsha@gmail.com>
2025-01-09 22:19:51 +0530

    week-5: backend initial setup

--- week-5/backend/index.js
+++ week-5/backend/index.js
@@ -1,1 +1,35 @@
-// start writing from here
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./db");
+const userRoutes = require("./routes/user");
+const todoRoutes = require("./routes/todo");
+const userMiddleware = require("./middleware/user");
+
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get("/healthy", (req, res) => res.send("I am Healthy"));
+app.use("/api/user", userRoutes);
+app.use("/api/todos", userMiddleware, todoRoutes);
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// database connection and server start
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
